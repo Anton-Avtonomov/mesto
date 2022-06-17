c15089ec2fa05977cbed5b2974e4759d8a7a0279
Avtonomov Anton <antonavtonomov@yandex.ru>
2022-06-17 14:33:58 +0400

    fix: popup and profile

--- script/script.js
+++ script/script.js
@@ -1,18 +1,39 @@
+//Opened and closing popUp by click
+
 const popUp = document.getElementById("popup");
-const buttonAdd = document.querySelector(".button__add");
-const buttonEdit = document.querySelector(".button__edit");
-const buttonSave = document.querySelector(".popup__button-save");
-let buttonClose = document.querySelector('.popup__button-close');
+const buttonEditPopUp = document.querySelector(".profile__button-edit");
+const buttonClosePopUp = document.querySelector(".popup__button-close");
+//console.log(popUp, buttonEditPopUp, buttonClosePopUp);
 
-console.log(popUp, buttonAdd, buttonEdit, buttonSave, buttonClose);
-
-buttonEdit.addEventListener('click', function(e) {
+buttonEditPopUp.addEventListener('click', function(e) {
     e.preventDefault();
     console.log('Пользователь нажал(а) на кнопку редактирования профиля');
-    popUp.classList.add('popup__active');
+    popUp.classList.add('popup_opened');
 })
 
-buttonClose.addEventListener('click', () => {
+buttonClosePopUp.addEventListener('click', () => {
     console.log('Пользователь нажал(а) на кнопку закрытия попАп');
-    popUp.classList.remove('popup__active');
+    popUp.classList.remove('popup_opened');
 })
+
+//Save value form
+
+const formElement = document.querySelector(".popup__content");
+console.log(formElement);
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    let namePopUp = document.querySelector(".popup__input_value_name");
+    const valueNamePopUp = namePopUp.value;
+    let aboutHimPopUp = document.querySelector(".popup__input_value_about-him");
+    const valueAboutHimPopUp = aboutHimPopUp.value;
+    //console.log(valueNamePopUp, valueAboutHimPopUp);
+
+    const profileName = document.querySelector(".profile__name");
+    const profileAboutHim = document.querySelector(".profile__about-him");
+    //console.log(profileName, profileAboutHim);
+
+    profileName.textContent = valueNamePopUp;
+    profileAboutHim.textContent = valueAboutHimPopUp;
+}
+formElement.addEventListener('submit', formSubmitHandler);
