9ad69263ebc45ad0a05fc9524fff5f0c4ae7c378
Avtonomov Anton <antonavtonomov@yandex.ru>
2022-06-17 16:01:08 +0400

    feat: function save value popUp

--- script/script.js
+++ script/script.js
@@ -1,39 +1,48 @@
 //Opened and closing popUp by click
 
-const popUp = document.getElementById("popup");
-const buttonEditPopUp = document.querySelector(".profile__button-edit");
-const buttonClosePopUp = document.querySelector(".popup__button-close");
+const popUp = document.getElementById('popup');
+const buttonEditPopUp = document.querySelector('.profile__button-edit');
+const buttonClosePopUp = document.querySelector('.popup__button-close');
 //console.log(popUp, buttonEditPopUp, buttonClosePopUp);
 
-buttonEditPopUp.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('Пользователь нажал(а) на кнопку редактирования профиля');
+buttonEditPopUp.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    console.log('Пользователь нажал(а) на кнопку редактирования профиля!');
     popUp.classList.add('popup_opened');
 })
 
 buttonClosePopUp.addEventListener('click', () => {
-    console.log('Пользователь нажал(а) на кнопку закрытия попАп');
+    console.log('Пользователь нажал(а) на кнопку закрытия попАп!');
     popUp.classList.remove('popup_opened');
 })
 
 //Save value form
-
-const formElement = document.querySelector(".popup__content");
+const formElement = document.querySelector('.popup__content');
 console.log(formElement);
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let namePopUp = document.querySelector(".popup__input_value_name");
+    let namePopUp = document.querySelector('.popup__input_value_name');
     const valueNamePopUp = namePopUp.value;
-    let aboutHimPopUp = document.querySelector(".popup__input_value_about-him");
+    let aboutHimPopUp = document.querySelector('.popup__input_value_about-him');
     const valueAboutHimPopUp = aboutHimPopUp.value;
+    //ПРОВЕРКА
     //console.log(valueNamePopUp, valueAboutHimPopUp);
 
-    const profileName = document.querySelector(".profile__name");
-    const profileAboutHim = document.querySelector(".profile__about-him");
+    const profileName = document.querySelector('.profile__name');
+    const profileAboutHim = document.querySelector('.profile__about-him');
+    //ПРОВЕРКА
     //console.log(profileName, profileAboutHim);
 
     profileName.textContent = valueNamePopUp;
     profileAboutHim.textContent = valueAboutHimPopUp;
+
+    //Closed popUp
+    const buttonSaveForm = document.querySelector('.popup__button-save');
+    buttonSaveForm.addEventListener('click', function() {
+        console.log('Пользователь сохранил(а) форму!');
+        popUp.classList.remove('popup_opened');
+    })
+
 }
 formElement.addEventListener('submit', formSubmitHandler);
