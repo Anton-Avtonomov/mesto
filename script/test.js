f90e5d5d3456bfb262f34aad9f080c6de327cab4
Anton Avtonomov <antonavtonomov@yandex.ru>
2022-06-29 12:47:37 +0300

    feat:button add create plae card

--- script/test.js
+++ script/test.js
@@ -1,114 +1,118 @@
-// VARIABLES
-const profileForm = document.querySelector('.popup__form_place_profile');
+// !VARIABLES
 // const placeForm = document.querySelector('.popup__form_place_card-place');
-let popUpInputName = document.querySelector('.popup__input_value_name');
-let popUpInputAboutHim = document.querySelector('.popup__input_value_about-him');
+const profileForm = document.querySelector('.popup__form_profile_form');
 const profileName = document.querySelector('.profile__name');
 const profileAboutHim = document.querySelector('.profile__about-him');
+const popUps = document.querySelectorAll('.popup');
 const popUpProfile = document.getElementById('popup-profile');
+let popUpProfileInputName = document.querySelector('.popup__input_string_name');
+let popUpProfileInputAboutHim = document.querySelector('.popup__input_string_about-him');
 const popUpPlace = document.getElementById('popup-place');
+let popUpPlaceInputTitleImage = document.querySelector('.popup__input_card-place_title');
+let popUpPlaceInputLinkImage = document.querySelector('.popup__input_card-place_link');
 const buttonEditPopUpProfile = document.querySelector('.profile__button-edit');
 const buttonAddCardPlace = document.querySelector('.profile__button-add');
-const popUps = document.querySelectorAll('.popup');
 const buttonClosePopUp = document.querySelectorAll('.popup__button-close');
 const buttonsLike = document.querySelectorAll('.element__logo-like');
-const buttonDeletePlace = document.querySelectorAll('.element__logo-delete');
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
+const buttonDeleteCardPlace = document.querySelectorAll('.element__logo-delete');
+const templateCardPlace = document.getElementById('template-card-place').content;
 
 
-// FUNCTION
+
+
+
+// !FUNCTION
 function openPopUpProfile() {
-    popUpProfile.classList.add('popup_opened');
-    popUpInputName.value = profileName.textContent;
-    popUpInputAboutHim.value = profileAboutHim.textContent;
-    console.log('Пользователь нажал(а) на кнопку редактирования профиля!');
+	popUpProfile.classList.add('popup_opened');
+	// Дублируем значения в поля попапа при открытии
+	popUpProfileInputName.value = profileName.textContent;
+	popUpProfileInputAboutHim.value = profileAboutHim.textContent;
+	console.log('Пользователь нажал(а) на кнопку редактирования профиля!');
 }
 
 function openPopUpPlace() {
-    popUpPlace.classList.add('popup_opened');
-    console.log('Пользователь нажал(а) на кнопку добавления карточки места!');
+	popUpPlace.classList.add('popup_opened');
+	//  Обнуляем значения при повторном добавлении карточки
+	popUpPlaceInputTitleImage.value = '';
+	popUpPlaceInputLinkImage.value = '';
+	console.log('Пользователь нажал(а) на кнопку добавления карточки места!');
 }
 
 function closePopUp() {
-    popUps.forEach(function (popUp) {
-        popUp.classList.remove('popup_opened');
-        console.log('Пользователь нажал(а) на кнопку закрытия попАпа!');
-    });
+	popUps.forEach(function (popUp) {
+		popUp.classList.remove('popup_opened');
+		console.log('Пользователь нажал(а) на кнопку закрытия попАпа!');
+	});
 }
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    console.log('Пользователь нажал(а) на кнопку сохранения профиля!');
-    profileName.textContent = popUpInputName.value;
-    profileAboutHim.textContent = popUpInputAboutHim.value;
-    closePopUp();
+	evt.preventDefault();
+	console.log('Пользователь нажал(а) на кнопку сохранения профиля!');
+	profileName.textContent = popUpProfileInputName.value;
+	profileAboutHim.textContent = popUpProfileInputAboutHim.value;
+	closePopUp();
 }
-//Кнопка Like
+
+
+
+
+// !Кнопка Like
 //Методом ForEach обходим коллекцию и получаем элементы c именем "btn" (функция callBack)
 buttonsLike.forEach(function (btn) {
-    //Элементу "btn" добавляем слушатель по событию клика 
-    btn.addEventListener('click', function (evt) {
-        //Добавление и удаление класса по клику кнопке лайка
-        evt.target.classList.toggle('element__logo-like_active');
-        console.log(`Пользователю понравилась карточка ${evt.target}`);
-    })
+	//Элементу "btn" добавляем слушатель по событию клика 
+	btn.addEventListener('click', function (evt) {
+		//Добавление и удаление класса по клику кнопке лайка
+		evt.target.classList.toggle('element__logo-like_active');
+		console.log(`Пользователю понравилась карточка ${evt.target}`);
+	})
 })
 
-// Кнопка удалить место
+// !Кнопка удалить место
 //Методом ForEach обходим коллекцию и получаем элементы c именем "btn" (функция callBack)
-buttonDeletePlace.forEach(function (btn) {
-    //Элементу "btn" добавляем слушатель по событию клика 
-    btn.addEventListener('click', function (evt) {
-        //Элемент с которым работает функция.Отслеживаем кнопки на которую нажали.Ищем его родительский элемент.Удаляем карточку места из дома
-        evt.target.closest('.element').remove();
-        console.log('Пользователь нажал(а) на кнопку удаления корточки места!');
-    })
+buttonDeleteCardPlace.forEach(function (btn) {
+	//Элементу "btn" добавляем слушатель по событию клика 
+	btn.addEventListener('click', function (evt) {
+		//Элемент с которым работает функция.Отслеживаем кнопки на которую нажали.Ищем его родительский элемент.Удаляем карточку места из дома
+		evt.target.closest('.element').remove();
+		console.log('Пользователь нажал(а) на кнопку удаления корточки места!');
+	})
 })
 
-// Открытие ПопАп карточки места
-const buttonsOpenPopUpCardPlace = document.querySelectorAll('.element');
+// !Открытие ПопАп карточки места
+const buttonsOpenPopUpCardPlace = document.querySelectorAll('.element__image');
 const popUpCardPlace = document.getElementById('popup-card-place');
 
 function openPopUpCardPlace() {
-    popUpCardPlace.classList.add('popup_opened');
+	popUpCardPlace.classList.add('popup_opened');
+
 }
-buttonsOpenPopUpCardPlace.forEach(function(btn) {
-    btn.addEventListener('click', openPopUpCardPlace);
+buttonsOpenPopUpCardPlace.forEach(function (btn) {
+	btn.addEventListener('click', openPopUpCardPlace);
 })
 
+// !Добавление карточки
+const blockCrads = document.querySelector('.elements');
+const buttonCreateCardPlace = document.querySelector('.popup__button_action_create');
 
-// LISTENER
+buttonCreateCardPlace.addEventListener('click', createNewCardPlace);
+
+function createNewCardPlace() {
+	const newCardPlace = templateCardPlace.querySelector('.element').cloneNode(true);
+	newCardPlace.querySelector('.element__title').textContent = popUpPlaceInputTitleImage.value;
+	newCardPlace.querySelector('.element__image').src = popUpPlaceInputLinkImage.value;
+	blockCrads.append(newCardPlace);
+	closePopUp();
+}
+// function renderCARD(data) {
+// 	const card = d
+// }
+
+// !LISTENER
 profileForm.addEventListener('submit', formSubmitHandler);
 buttonEditPopUpProfile.addEventListener('click', openPopUpProfile);
 buttonAddCardPlace.addEventListener('click', openPopUpPlace);
 buttonClosePopUp.forEach(function (btn) {
-    btn.addEventListener('click', closePopUp);
+	btn.addEventListener('click', closePopUp);
 });
 
 
@@ -128,4 +132,3 @@ buttonClosePopUp.forEach(function (btn) {
 // //поиск всех элементов с указанным селектором - создание коллекции
 // let exampleItem = document.querySelectorAll('.example__item');
 // console.log(exampleItem);
-
