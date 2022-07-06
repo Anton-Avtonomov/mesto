45a88bd9e8f6dc370f2bde4f76eefd15005d59d4
Avtonomov Anton <antonavtonomov@yandex.ru>
2022-07-06 17:09:52 +0400

    hotfix: redaction index.js before review number two

--- script/index.js
+++ script/index.js
@@ -11,14 +11,6 @@ const buttonCreateNewCardPlace = document.querySelector('.popup__button_action_c
 const buttonEditPopUpProfile = document.querySelector('.profile__button-edit');
 // Коллекция кнопок закрытия Попапов
 const buttonsClosePopUp = document.querySelectorAll('.popup__button-close');
-
-// // Кнопка закрытия попапа профиля
-// const buttonClosePopUpProfile = document.getElementById('button-close-popup-profile');
-// // Кнопка закрытия попапа формы добавлени карточки
-// const buttonClosePopUpPlace = document.getElementById('button-close-popup-place');
-// // Кнопка закрытия попапа карточки места
-// const buttonClosePopUpCardPlace = document.getElementById('button-close-popup-card-place');
-
 // Коллекция кнопок открытия попАпов карточек мест
 const buttonsShowPopUpCardPlace = document.querySelectorAll('.element__image');
 // Имя профиля
@@ -52,176 +44,161 @@ const popUpTitlePhoto = document.querySelector('.popup__image-title');
 
 // !Функция открытия попапа
 function openPopup(popup) {
-	popup.classList.add('popup_opened');
-	// console.log('Сработала функция открытия попАпа');
+    popup.classList.add('popup_opened');
+    // console.log('Сработала функция открытия попАпа');
 };
+
 // !Открытие попапа профиля
-function openPopUpProfile() {
-	openPopup(popUpProfile);
-	// Дублируем значения в поля попапа при открытии
-	popUpProfileName.value = profileName.textContent;
-	popUpProfileInfo.value = profileInfo.textContent;
-	// Сохранения формы профиля
-	formProfile.addEventListener('submit', handleSubmitformProfile);
-	// console.log('Сработала функция открытия попАпа профиля')
-};
 // Кнопка редактирования профиля
 buttonEditPopUpProfile.addEventListener('click', openPopUpProfile);
 
-// !Открытие попапа добавления карточки
-function openPopUpPlace() {
-	openPopup(popUpPlace);
-	// Reset формы при каждом открытии попапа
-	formPlace.reset();
-	// console.log(`Пользователь открыл(а) ${popUpPlace.id}`);
+function openPopUpProfile() {
+    openPopup(popUpProfile);
+    // Дублируем значения в поля попапа при открытии
+    popUpProfileName.value = profileName.textContent;
+    popUpProfileInfo.value = profileInfo.textContent;
+    // console.log('Сработала функция открытия попАпа профиля')
 };
+
+function handleSubmitformProfile(event) {
+    event.preventDefault();
+    profileName.textContent = popUpProfileName.value;
+    profileInfo.textContent = popUpProfileInfo.value;
+    closePopUp(popUpProfile);
+    // console.log(`Сработал submit в ${popUpProfile.id}!`);
+};
+//Сохранения формы профиля
+formProfile.addEventListener('submit', handleSubmitformProfile);
+
+// !Открытие попапа добавления карточки
 // Кнопка +
 buttonOpenPopUpPlace.addEventListener('click', openPopUpPlace);
 
+function openPopUpPlace() {
+    openPopup(popUpPlace);
+    // Reset формы при каждом открытии попапа
+    formPlace.reset();
+    // console.log(`Пользователь открыл(а) ${popUpPlace.id}`);
+};
+
+//Сохранения формы места
+function handleSubmitFormPlace(event) {
+    event.preventDefault();
+    closePopUp(popUpPlace);
+    // console.log(`Сработал submit в ${popUpPlace.id}!`);
+};
+formPlace.addEventListener('submit', handleSubmitFormPlace);
+
 // !Открытие попапа карточки места
 function openPopUpCardPlace(event) {
-	openPopup(popUpCardPlace);
-	popUpPhoto.src = event.target.src;
-	popUpPhoto.alt = event.target.alt;
-	formPlace.addEventListener('submit', handleSubmitformProfile);
-	popUpTitlePhoto.textContent = event.target.parentElement.querySelector('.element__title').textContent;
-	// console.log(`Пользователь открыл(а) ${event.target.parentNode.querySelector('.element__title').textContent}`);
+    openPopup(popUpCardPlace);
+    popUpPhoto.src = event.target.src;
+    popUpPhoto.alt = event.target.alt;
+    popUpTitlePhoto.textContent = event.target.closest('.element').querySelector('.element__title').textContent;
+    // console.log(`Пользователь открыл(а) карточку ${event.target.closest('.element').querySelector('.element__title').textContent}`);
 };
-// Кнопки открытия попАпа карточки места
-buttonsShowPopUpCardPlace.forEach(function (btn) {
-	btn.addEventListener('click', openPopUpCardPlace);
-});
 
 // !Закрытие попапов
 // Функция закрытия попапа
 function closePopUp(popup) {
-	popup.classList.remove('popup_opened');
-	formPlace.removeEventListener('submit', handleSubmitformProfile);
-	// console.log('Сработала функция закрытия попАпа!');
+    popup.classList.remove('popup_opened');
+    // console.log('Сработала функция закрытия попАпа!');
 };
-const handleClosePop = function (event) {
-	if (event.target === event.target.closest('.popup__button-close')) {
-		closePopUp(event.target.closest('.popup'));
-		// console.log(`Пользователь закрыл ${event.target.closest('.popup').id}`);
-	};
+
+const handleClosePop = function(event) {
+    if (event.target.classList.contains('popup__button-close')) {
+        closePopUp(event.target.closest('.popup'));
+        // console.log(`Пользователь закрыл ${event.target.closest('.popup').id}`);
+    };
 };
-popUps.forEach(function (popup) {
-	popup.addEventListener('click', handleClosePop);
+
+popUps.forEach(function(popup) {
+    popup.addEventListener('click', handleClosePop);
 });
-// // !Закрытие попапа карточки места
-// function closePopUpCardPlace() {
-// 	closePopUp(popUpCardPlace);
-// }
-// buttonClosePopUpCardPlace.addEventListener('click', closePopUpCardPlace);
-
-// // Альтернативный код, но при его использование событие зактытия будет срабатывать по всем попапам
-// buttonsClosePopUp.forEach(function (btn) {
-// 	btn.addEventListener('click', closePopUp);
-// });
-
-// function closePopUp(popup) {
-// 	popUps.forEach(function (popup) {
-// 		popup.classList.remove('popup_opened');
-// 		console.log(`Пользователь нажал(а) на кнопку закрытия попАпа ${popup.id}!`);
-// 	});
-// }
-
-// // Альтернативный код закрытия попапов, каждого по отедльности
-// // !Закрытие попапа профиля
-// function closePopUpProfile() {
-// 	closePopUp(popUpProfile);
-// }
-// buttonClosePopUpProfile.addEventListener('click', closePopUpProfile);
-
-// // !Закрытие попапа добавления карточки
-// function closePopUpPlace() {
-// 	closePopUp(popUpPlace);
-// }
-// buttonClosePopUpPlace.addEventListener('click', closePopUpPlace);
-
-// !Событие submit формы профиля
-function handleSubmitformProfile(event) {
-	event.preventDefault();
-	profileName.textContent = popUpProfileName.value;
-	profileInfo.textContent = popUpProfileInfo.value;
-	closePopUp(popUpProfile);
-	// console.log(`Сработал submit в ${popUpProfile.id}!`);
-};
 
 // !Функция добавления карточки места
-function createCard() {
-	const contentTemplateCardPlace = templateCardPlace.cloneNode(true);
-	setEventListener(contentTemplateCardPlace);
-	console.log('функция создания новой карточки');
-	return contentTemplateCardPlace;
-}
+
+//function createCard(oneParametr, twoParametr, threeParametr) {
+//    const contentTemplateCardPlace = templateCardPlace.cloneNode(true);
+//    setEventListener(contentTemplateCardPlace);
+//    contentTemplateCardPlace.querySelector('.element__image').src = oneParametr;
+//    contentTemplateCardPlace.querySelector('.element__image').alt = twoParametr;
+//    contentTemplateCardPlace.querySelector('.element__title').textContent = threeParametr;
+//    console.log('Выполнена функция создания карточки');
+//    return contentTemplateCardPlace;
+//}
+//initialCardsPlace.forEach(function(dataCardObject) {
+//    createCard(dataCardObject.link, dataCardObject.alt, dataCardObject.name);
+//    blockCardsPlace.prepend(dataCardObject);
+//    console.log('добавлена новая карточка');
+//})
 
 // !!!Далее не могу реализовать, е понимаю как нужны подсказки
 function renderingCardPlace(objectCardPlace) {
-	createCard();
-	// Клонирую карточку для добавления туда контента
-	const contentTemplateCardPlace = templateCardPlace.cloneNode(true);
-	const templateImage = contentTemplateCardPlace.querySelector('.element__image');
-	// Нахожу в созданной копии карточки атрибут src элемента изображения и присваиваю его параметру функции - card
-	templateImage.src = objectCardPlace.link;
-	templateImage.alt = objectCardPlace.alt;
-	contentTemplateCardPlace.querySelector('.element__title').textContent = objectCardPlace.name;
-	// Вешаем слушатели карточки
-	setEventListener(contentTemplateCardPlace);
-	// Добавляем карточку в начало DOM контейнера с карточками
-	blockCardsPlace.prepend(contentTemplateCardPlace);
-	// console.log('Выполнена функция добавления карточки места');
+    // Клонирую карточку для добавления туда контента
+    const contentTemplateCardPlace = templateCardPlace.cloneNode(true);
+    const templateImage = contentTemplateCardPlace.querySelector('.element__image');
+    // Нахожу в созданной копии карточки атрибут src элемента изображения и присваиваю его параметру функции - card
+    templateImage.src = objectCardPlace.link;
+    templateImage.alt = objectCardPlace.alt;
+    contentTemplateCardPlace.querySelector('.element__title').textContent = objectCardPlace.name;
+    // // Вешаем слушатели карточки
+    setEventListener(contentTemplateCardPlace);
+    // Добавляем карточку в начало DOM контейнера с карточками
+
+    blockCardsPlace.prepend(contentTemplateCardPlace);
+    // console.log('Выполнена функция добавления карточки места');
 };
 
 // !Функция добавления новой карточки в DOM
 function createNewCardPlace() {
-	if (inputTitlePhoto.value.length !== 0 && inputLinkPhoto.value.length !== 0) {
-		const contentTemplateCardPlace = templateCardPlace.cloneNode(true);
-		const templateImage = contentTemplateCardPlace.querySelector('.element__image');
-		templateImage.src = inputLinkPhoto.value;
-		templateImage.alt = `Фотография ${inputTitlePhoto.value}`;
-		contentTemplateCardPlace.querySelector('.element__title').textContent = inputTitlePhoto.value;
-		setEventListener(contentTemplateCardPlace);
-		blockCardsPlace.prepend(contentTemplateCardPlace);
-		closePopUp(popUpPlace);
-		// console.log('Пользователь добавил новую карточку места');
-	} else {
-		alert('Необходимо заполнить все поля!');
-	}
-}
-
+    if (inputTitlePhoto.value.length !== 0 && inputLinkPhoto.value.length !== 0) {
+        const contentTemplateCardPlace = templateCardPlace.cloneNode(true);
+        const templateImage = contentTemplateCardPlace.querySelector('.element__image');
+        templateImage.src = inputLinkPhoto.value;
+        templateImage.alt = `Фотография ${inputTitlePhoto.value}`;
+        contentTemplateCardPlace.querySelector('.element__title').textContent = inputTitlePhoto.value;
+        setEventListener(contentTemplateCardPlace);
+        blockCardsPlace.prepend(contentTemplateCardPlace);
+        closePopUp(popUpPlace);
+        // console.log('Пользователь добавил новую карточку места');
+    }
+    // } else {
+    //     alert('Необходимо заполнить все поля!');
+    // }
+};
 // Кнопка создания новой карточки в форме 
 buttonCreateNewCardPlace.addEventListener('click', createNewCardPlace);
-// !Функция отметки like карточки по event
-function handleLike(event) {
-	event.target.classList.toggle('element__logo-like_active');
-	// console.log(`Пользователь нажал(а) поставил LIKE карточке ${event.target.parentNode.parentNode.querySelector('.element__title').textContent}`);
-};
 
 // !Функция удаления карточки по event
 function handleDelete(event) {
-	event.target.closest('.element').remove();
-	// console.log(`Пользователь нажал(а) на кнопку удаления карточки ${event.target.parentNode.parentNode.querySelector('.element__title').textContent}`);
+    event.target.closest('.element').remove();
+    // console.log(`Пользователь нажал(а) на кнопку удаления карточки ${event.target.closest('.element').querySelector('.element__title').textContent}`);
+};
+
+// !Функция отметки like карточки по event
+function handleLike(event) {
+    event.target.classList.toggle('element__logo-like_active');
+    // console.log(`Пользователь нажал(а) поставил LIKE карточке ${event.target.closest('.element').querySelector('.element__title').textContent}`);
 };
 
 // ! Вешаем слушатели
 function setEventListener(element) {
-	element.querySelector('.element__button-delete').addEventListener('click', handleDelete);
-	element.querySelector('.element__logo-like').addEventListener('click', handleLike);
-	element.querySelector('.element__image').addEventListener('click', openPopUpCardPlace);
-	console.log('Добавлены слушатели: Удаления карточек, Добавления лайка, открытия попАпа карточки места')
+    //Кнопка удаления
+    element.querySelector('.element__button-delete').addEventListener('click', handleDelete);
+    // Кнопка LIKE
+    element.querySelector('.element__logo-like').addEventListener('click', handleLike);
+    // Кнопка открытия попапа карточки
+    element.querySelector('.element__image').addEventListener('click', openPopUpCardPlace);
+    // console.log('Добавлены слушатели: Удаления карточек, Добавления лайка, открытия попАпа карточки места')
 };
 
 //! Загрузка карточек мест на страницу
 function loadCardsPlace() {
-	// Прохожусь функцией renderCardPlace по всем объектам массива с шаблонными карточками
-	initialCardsPlace.forEach(function (objectCardPlace) {
-		renderingCardPlace(objectCardPlace);
-	});
-	// console.log('Произошла загрузка ВСЕХ карточек места');
+    // Прохожусь функцией renderCardPlace по всем объектам массива с шаблонными карточками
+    initialCardsPlace.forEach(function(objectCardPlace) {
+        renderingCardPlace(objectCardPlace);
+    });
+    // console.log('Произошла загрузка ВСЕХ карточек места');
 };
 loadCardsPlace();
-
-
-
-
